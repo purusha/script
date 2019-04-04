41ac195ec8cdd1c94238031688c66a4b9b566510
alan toro <alanprog@gmail.com>
2019-04-04 16:57:39 +0200

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,1 @@
+tar -zcvf backup.tgz /home/developer/dev --exclude="**/.git/*" --exclude="**/target/*" --exclude="**/log/*" --exclude="**/.settings/*" --exclude="**/.classpath/*" --exclude="**/.mvn/*"
